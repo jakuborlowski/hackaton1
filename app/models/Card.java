1a4dbbf01ba8b37c0d1dae59b6ba831dba59efc3
Łukasz Stefaniak <lustefaniak@gmail.com>
2011-09-10 19:48:48 +0200

    Naprawienie problemu z błędami w modelu

--- app/models/Card.java
+++ app/models/Card.java
@@ -8,7 +8,9 @@ import play.db.jpa.*;
 @Entity
 public class Card extends Model {
 
+	@OneToMany
     public List<Change> required;
+	@OneToMany
     public List<Change> operations;
 
     public boolean isPossible() {
--- app/models/Castle.java
+++ app/models/Castle.java
@@ -8,13 +8,16 @@ import play.db.jpa.*;
 @Entity
 public class Castle extends Model {
 	
-	public Map<String, Integer> storage;
-	public Map<String, Integer> castle;
+	
+	//public Map<String, Integer> storage;
+	//public Map<String, Integer> castle;
+	
+	@OneToMany
 	public List<Card> cards;
 	
-	public void Castle()
+	public Castle()
 	{
-		this.storage = new HashMap<String, Integer>();
-		this.castle = new HashMap<String, Integer>();
+		//this.storage = new HashMap<String, Integer>();
+		//this.castle = new HashMap<String, Integer>();
 	}
 }
